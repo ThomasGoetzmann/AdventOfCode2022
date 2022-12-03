e554de4a69bef95764492a34b799b5e9a9b99d11
Thomas Goetzmann <thomas.goetzmann@gmail.com>
2022-12-03 10:36:05 +0100

    Day 3

--- sources/Program.fs
+++ sources/Program.fs
@@ -1,5 +1,6 @@
 ﻿open Year2022Day1
 open Year2022Day2
+open Year2022Day3
 
 printfn "Hello from F#"
 printfn "Day 1 part 1 solution is %i" SolveDay1Part1
@@ -7,3 +8,6 @@ printfn "Day 1 part 2 solution is %i" SolveDay1Part2
 
 printfn "Day 2 part 1 solution is %i" SolveDay2Part1
 printfn "Day 2 part 2 solution is %i" SolveDay2Part2
+
+printfn "Day 3 part 1 solution is %i" SolveDay3Part1
+printfn "Day 3 part 2 solution is %i" SolveDay3Part2
