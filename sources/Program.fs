9d4d31fef32e87c98e00464abc272cec684ec280
Thomas Goetzmann <thomas.goetzmann@gmail.com>
2022-12-04 14:30:16 +0100

    Day 4

--- sources/Program.fs
+++ sources/Program.fs
@@ -1,6 +1,7 @@
 ﻿open Year2022Day1
 open Year2022Day2
 open Year2022Day3
+open Year2022Day4
 
 printfn "Hello from F#"
 printfn "Day 1 part 1 solution is %i" SolveDay1Part1
@@ -11,3 +12,6 @@ printfn "Day 2 part 2 solution is %i" SolveDay2Part2
 
 printfn "Day 3 part 1 solution is %i" SolveDay3Part1
 printfn "Day 3 part 2 solution is %i" SolveDay3Part2
+
+printfn "Day 4 part 1 solution is %i" SolveDay4Part1
+printfn "Day 4 part 2 solution is %i" SolveDay4Part2
---
+++ sources/Year2022Day4.fs
@@ -0,0 +1,45 @@
+module Year2022Day4
+
+open System.IO
+
+let inputs = File.ReadAllLines("inputs/day4.txt")
+
+let parse (line: string) =
+    let splits = line.Split(',')
+
+    let firstElf = splits.[0].Split('-')
+    let secondElf = splits.[1].Split('-')
+
+    let firstSections =
+        [ (firstElf.[0] |> int) .. (firstElf.[1] |> int) ]
+
+    let secondSections =
+        [ (secondElf.[0] |> int) .. (secondElf.[1] |> int) ]
+
+    (firstSections, secondSections)
+
+let fullyContains (range1, range2) =
+    let set1 = range1 |> Set.ofList
+    let set2 = range2 |> Set.ofList
+
+    (set1 |> Set.isSubset set2)
+    || (set1 |> Set.isSuperset set2)
+
+let overlap (range1, range2) =
+    let set1 = range1 |> Set.ofList
+    let set2 = range2 |> Set.ofList
+
+    set1 |> Set.intersect set2 |> Set.isEmpty |> not
+
+
+let SolveDay4Part1 =
+    inputs
+    |> Seq.map parse
+    |> Seq.filter fullyContains
+    |> Seq.length
+
+let SolveDay4Part2 =
+    inputs
+    |> Seq.map parse
+    |> Seq.filter overlap
+    |> Seq.length
