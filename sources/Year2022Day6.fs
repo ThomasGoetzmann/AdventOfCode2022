fad75faa3e0a1c2cc1be189021e9017c22226454
Thomas Goetzmann <thomas.goetzmann@gmail.com>
2022-12-10 21:56:59 +0100

    Day 6

--- sources/Year2022Day6.fs
+++ sources/Year2022Day6.fs
@@ -2,8 +2,19 @@ module Year2022Day6
     open System.IO
     
     let inputs =
-        File.ReadAllText("inputs/day6.txt")
-
-    let part1 = 0
+        File.ReadAllText("inputs/day6.txt") 
+        |> List.ofSeq
     
-    let part2 = 0
+    let add x y = x + y
+
+    let analyze size input  =
+        input 
+        |> List.windowed size
+        |> List.findIndex (fun x -> (x |> List.distinct |> List.length) = size)
+        |> add size
+
+    let packet = 4
+    let message = 14
+
+    let part1 = inputs |> analyze packet
+    let part2 = inputs |> analyze message
