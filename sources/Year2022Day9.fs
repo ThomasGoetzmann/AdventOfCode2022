0419f6e418cdb3880471895df5592270b2b909f6
Thomas GOETZMANN <thomas.goetzmann.external-provider@kuhn.com>
2022-12-12 16:47:17 +0100

    Day 9 (needs some cleaning)

--- sources/Year2022Day9.fs
+++ sources/Year2022Day9.fs
@@ -25,7 +25,7 @@ let parse line =
     | "D" -> Down |> List.replicate number
     | _ -> failwith "Invalid input"
 
-let moveHead move position =
+let apply move position =
     let x, y = position
     match move with
     | Up -> x , y + 1
@@ -33,37 +33,59 @@ let moveHead move position =
     | Left -> x - 1, y
     | Right -> x + 1, y
 
-let moveTail pos1 pos2 =
+let follow pos1 pos2 =
     let x1, y1 = pos1
     let x2, y2 = pos2
     let xdiff, ydiff = x1 - x2 , y1 - y2
     
     match xdiff, ydiff with
+    | 2 , 2 ->  x2 + 1, y2 + 1
+    | 2 , -2 -> x2 + 1, y2 - 1
+    | -2 , 2 -> x2 - 1, y2 + 1
+    | -2 , -2 -> x2 - 1, y2 - 1
     | 2 , _ -> x2 + 1, y1
     | -2 , _ -> x2 - 1, y1
     | _ , 2 -> x1, y2 + 1
     | _ , -2 -> x1, y2 - 1
     | _ -> pos2
 
-let applyMoves moves = 
-    let rec applyMove acc head tail moves =
-        match moves with
-        | m :: ms -> 
-            let newHead = head |> moveHead m
-            let newTail = moveTail newHead tail
-            applyMove (newTail::acc) newHead newTail ms
-        | [] -> acc
+let rec applyMoves a rope moves =
     
-    applyMove [0,0] (0,0) (0,0) moves
+    let applyMove rope move  = 
+    
+        let rec followRope acc knots =
+            match knots with
+            | k::ks -> 
+                let movedKnot = k |> follow (acc |> List.head)
+                followRope (movedKnot::acc) ks
+            | [] -> acc |> List.rev
+        
+        let newHead = (rope |> List.head) |> apply move
+        followRope [newHead] rope.Tail
+
+    match moves with 
+    | m::ms -> 
+        let newRope = applyMove rope m
+        applyMoves (newRope::a) newRope ms
+    | [] -> a |> List.rev
+
 
 let part1 =
+    let rope = (0,0) |> List.replicate 2
+    
     inputs 
     |> List.collect parse
-    |> applyMoves
+    |> applyMoves [rope] rope
+    |> List.map (fun rope -> rope |> List.last)
     |> List.distinct
     |> List.length
 
 let part2 = 
+    let rope = (0,0) |> List.replicate 10
+    
     inputs 
     |> List.collect parse
-    |> applyMoves
+    |> applyMoves [rope] rope
+    |> List.map (fun rope -> rope |> List.last)
+    |> List.distinct
+    |> List.length
