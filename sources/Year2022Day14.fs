3c5e012dc8afcc290a9929844b20ee6999edac0c
Thomas Goetzmann <thomas.goetzmann@gmail.com>
2022-12-27 00:45:26 +0100

    Day 14 part 1

--- sources/Year2022Day14.fs
+++ sources/Year2022Day14.fs
@@ -1,39 +1,66 @@
 module Year2022Day14
 
 open System.IO
-open System.Text.RegularExpressions
 
-let inputs = File.ReadAllLines("inputs/day14test.txt")
+let inputs = File.ReadAllLines("inputs/day14.txt")
 
-let parse (line:string) =
-    let parsePoint (pointString:string) =
+let parse (line: string) =
+    let parsePoint (pointString: string) =
         let split = pointString.Split(',')
-        split[0] |> int , split[1] |> int
-    
+        split[0] |> int, split[1] |> int
+
     line.Split(" -> ")
     |> List.ofArray
     |> List.map parsePoint
 
-let createlist i j =
-    if i < j then [i..j] else [j..i]
+let createlist i j = if i < j then [ i..j ] else [ j..i ]
 
-let drawPoints (a,b) =
+let drawPoints (a, b) =
     let xs = createlist (fst a) (fst b)
     let ys = createlist (snd a) (snd b)
-    xs |> List.allPairs ys
-  
+    ys |> List.allPairs xs
+
 let draw path =
-    path 
+    path
     |> List.pairwise
     |> List.map drawPoints
     |> List.collect id
 
-let part1 = 
-    inputs
-    |> List.ofArray
-    |> List.map parse
-    |> List.map draw
-    |> List.collect id
-    |> List.distinct
+let drizzleSandOver rocks =
+    let initialPosition = 500, 0
+    let bottom = rocks |> List.maxBy snd |> snd
+
+    let rec drizzle (x, y) occupied =
+        if y > bottom then
+            None
+        elif occupied |> List.contains (x, y + 1) |> not then
+            drizzle (x, y + 1) occupied
+        elif occupied |> List.contains (x - 1, y + 1) |> not then
+            drizzle (x - 1, y + 1) occupied
+        elif occupied |> List.contains (x + 1, y + 1) |> not then
+            drizzle (x + 1, y + 1) occupied
+        else
+            Some(x, y)
+
+    let rec makeItRainOver rocksAndSands =
+        match rocksAndSands |> drizzle initialPosition with
+        | Some sand -> makeItRainOver (sand :: rocksAndSands)
+        | None -> rocksAndSands
+
+    makeItRainOver rocks
+
+let part1 =
+    let rocks =
+        inputs
+        |> List.ofArray
+        |> List.map parse
+        |> List.map draw
+        |> List.collect id
+        |> List.distinct
+
+    let rocksAndSands = drizzleSandOver rocks
+
+    (rocksAndSands |> List.length)
+    - (rocks |> List.length)
 
 let part2 = 0
