cf0cfb4abeb2c3751b16ea8f1e91af734bf753ba
Thomas GOETZMANN <goetzmann.t@sfeir.com>
2022-12-26 08:19:58 +0100

    Thinking about day 13

--- sources/Year2022Day13.fs
+++ sources/Year2022Day13.fs
@@ -1,11 +1,30 @@
 module Year2022Day13
 
 open System.IO
-open System.Environment
 
 let newline = System.Environment.NewLine
 
-let parseLine (line: string) = line
+let (|Integer|_|) (str: string) =
+    let mutable intvalue = 0
+
+    if System.Int32.TryParse(str, &intvalue) then
+        Some(intvalue)
+    else
+        None
+
+let parseLine (line:string) =
+    
+    let rec parseStr (acc: list) (str : string) =
+        match str with
+        | '['::rest -> 
+            let list, rest = parseStr [] rest 
+            parseStr  (list @ acc) rest
+        | ']'::rest -> (acc |> List.rev) rest
+        | ','::rest -> parseStr acc rest
+        | i::rest when Integer i -> parseStr (i::acc) rest 
+        | _ -> acc, []
+
+    parseStr (line |> Seq.toList) []
 
 let inputs =
     File
